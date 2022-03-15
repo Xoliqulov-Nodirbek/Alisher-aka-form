601fdbd268cbdc7f6a2d362d14c4ae940588b028
Xoliqulov-Nodirbek <xoliqulov.nodirbek2003@gmail.com>
2022-03-15 23:10:21 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let elForm = document.querySelector(".form");
+let elInput = document.querySelector(".form__input");
+let elBtn = document.querySelector(".form__btn");
+
+let elResultUsd = document.querySelector(".result__usd");
+let elResultEuro = document.querySelector(".result__euro");
+let elResultRubl = document.querySelector(".result__rubl");
+
+let RUBL_TO_UZS = 93;
+let EURO_TO_UZS = 	11600;
+let DOLLAR_TO_UZS = 11130;
+
+elForm.addEventListener("submit", function(evt){
+    evt.preventDefault();
+    
+    let elInputVal = Math.round(elInput.value);
+    let rubl = (elInputVal / RUBL_TO_UZS).toFixed(2);
+    let euro = (elInputVal / EURO_TO_UZS).toFixed(2);
+    let dollar = (elInputVal / DOLLAR_TO_UZS).toFixed(2);
+    
+    elResultUsd.textContent += dollar;
+    elResultEuro.textContent += euro;
+    elResultRubl.textContent += rubl;
+    
+    if (elInputVal <= 0) {
+        alert("Iltimos, Alisher aka siz kursni bilishingiz uchun 0 dan farqli son kiriting !");
+    }
+    if (isNaN(elInputVal)) {
+        alert("Iltimos raqam kiriting !");
+    }
+})
